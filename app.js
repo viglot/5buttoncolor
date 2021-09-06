77a0778dbc6b73151785230cbd6d160ae050759c
Viglot <vigviglot36.hiltunen@gmail.com>
2021-09-06 09:22:46 +0300

    start

---
+++ app.js
@@ -0,0 +1,20 @@
+function chBackcolor(red) {
+    document.body.style.background = red;
+ }
+
+ function chBackcolor(green) {
+    document.body.style.background = green;
+ }
+
+ function chBackcolor(blue) {
+    document.body.style.background = blue;
+ }
+
+ function chBackcolor(black) {
+    document.body.style.background = black;
+ }
+
+ function chBackcolor(white) {
+    document.body.style.background = white;
+ }
+
